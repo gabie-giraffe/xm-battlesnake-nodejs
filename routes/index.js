5b6f6cc21612aaa1e37851f58e76df7f65e835ba
Gabrielle Gasse <ggasse@xmatters.com>
2019-07-11 11:37:24 -0700

    GameCOntext

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
-var express = require('express')
-var router  = express.Router()
+const express = require('express');
+const router = express.Router();
+const GameContext = require('../GameContext')
+
+console.log('>> module initializing')
+var gameContext = null;
 
 // Handle POST request to '/start'
-router.post('/start', function (req, res) {
+router.post('/start', (req, res) => {
   // NOTE: Do something here to start the game
- console.log(req.body)
+  gameContext = new GameContext(req.body);
   // Response data
-  var data = {
+  let data = {
     color: "#DFFF00",
-    name: "ContraSnake",
+    name: "Lucky team 13",
     head_url: "http://www.placecage.com/c/200/200", // optional, but encouraged!
     taunt: "I won't run into walls and i am made of javascript", // optional, but encouraged!
   }
@@ -17,44 +21,14 @@ router.post('/start', function (req, res) {
 })
 
 // Handle POST request to '/move'
-router.post('/move', function (req, res) {
-  // NOTE: Do something here to generate your move
-   //console.log(req.body)
-   let move = findFood(req.body)
+router.post('/move', (req, res) => {
   // Response data
-  var data = {
-    move: move, // one of: ['up','down','left','right']
+  let data = {
+    move: gameContext.nextMove(req.body), // one of: ['up','down','left','right']
     taunt: 'Outta my way, snake!', // optional, but encouraged!
   }
 
   return res.json(data)
 })
-function getMySnake(gameState){
-  return getSnake(gameState,gameState.you)
-}
-function getSnake(gameState, id){
-  let snake = gameState.snakes.find(snake => snake.id === id )
-  return snake;
-}
-function findFood(gameState) {
 
-  let mySnake = getMySnake(gameState) 
-  let head = mySnake.coords[0];
-        if (gameState.food[0][0] < head[0]) {
-            move = "left"
-        }
-
-        if (gameState.food[0][0] > head[0]) {
-          move = "right"
-        }
-
-        if (gameState.food[0][1] < head[1]) {
-          move = "up"
-        }
-
-        if (gameState.food[0][1] > head[1]) {
-          move = "down"
-        }
-        return move ;
-}
 module.exports = router
